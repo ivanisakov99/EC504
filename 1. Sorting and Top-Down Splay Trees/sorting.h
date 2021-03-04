5d08cd41a836918af3250026ad0411c98a3abc4d
ivanisakov99 <60196280+ivanisakov99@users.noreply.github.com>
2021-03-03 19:05:40 -0500

    Add files via upload

---
+++ 1. Sorting and Top-Down Splay Trees/sorting.h
@@ -0,0 +1,235 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+
+using namespace std;
+
+static int swapCount = 0;
+
+void swap(int* a, int* b)
+{
+    //swapCount++;
+    int temp;
+    temp = *a; 
+    *a = *b; 
+    *b = temp; 
+}
+
+
+/* See Knuth's shuffles https://en.wikipedia.org/wiki/Random_permutation */
+
+
+
+void insertionsort(int a[],  int n){
+  int i, key, j;  
+  for (i = 1; i < n; i++){  
+    key = a[i];  
+    j = i - 1;  
+    
+    while (j >= 0 && a[j] > key) {  
+      a[j + 1] = a[j];  
+      j = j - 1;  
+    }  
+    a[j + 1] = key;  
+  }  
+}
+
+// Order Theta(NlogN) sorting
+void mergeSort(int a[], int a_tmp[], int l, int r)
+{
+  // you program this.  a_tmp can be used for merging 
+  int i, j, k;
+  if(r > l){
+    int m = (l + r)/2;
+
+    mergeSort(a, a_tmp, l, m);
+    mergeSort(a, a_tmp, m + 1, r);
+
+    for(i = m + 1; i > l; i--){
+      a_tmp[i - 1] = a[i - 1];
+    }
+    for(j = m; j < r; j++){
+      a_tmp[r + m - j] = a[j + 1];
+    }
+    for(k = l; k <= r; k++){
+      a[k] = (a_tmp[i] < a_tmp[j]) ? a_tmp[i++] : a_tmp[j--];
+    }
+  }
+    
+}
+// 
+void mergeSortblend(int a[], int a_tmp[], int l, int r)
+{
+  int i, j, k;
+  if(r <= l){
+    return;
+  }
+ // You program this
+ if(r - l > 32){
+  int m = (l + r)/2;
+  mergeSortblend(a, a_tmp, l, m);
+  mergeSortblend(a, a_tmp, m + 1, r);
+
+    for(i = m + 1; i > l; i--){
+      a_tmp[i - 1] = a[i - 1];
+    }
+    for(j = m; j < r; j++){
+      a_tmp[r + m - j] = a[j + 1];
+    }
+    for(k = l; k <= r; k++){
+      a[k] = (a_tmp[i] < a_tmp[j]) ? a_tmp[i++] : a_tmp[j--];
+    }
+ }
+ else{
+   for(i = l + 1; i <= r; i++){
+      k = a[i];
+      j = i - 1;
+
+      while(j >= l && a[j] > k){
+        a[j + 1] = a[j];
+        j--;
+      }
+      a[j + 1] = k;
+   }
+ }
+
+}
+
+bool checkInorder(int a[],int left, int right){
+  //checks  that a is in order from left to right
+  int tmp;
+  for (int i = left; i < right; i++){
+    if (a[i] > a[i+1]){
+      tmp = i;
+      
+    }
+  }
+
+  return true;
+}
+
+void simpleTimsort(int a[], int a_tmp[], int n)
+{ 
+   //You program this
+  const int RUN = 32;
+  int runstack[n/32], num_in_stack = 0;
+  runstack[num_in_stack] = 0;
+  int i, j, z, key, l, m, r;
+  int secondLast, Last;
+
+  for(z = 1; z < n; z++){
+    if(a[z] > a[z - 1]){
+      continue;
+    }
+    else{
+      //If the size of the run is less than minrun
+      if( (z - runstack[num_in_stack]) < RUN){
+        //Insertion
+        key = a[z];
+        j = z - 1;
+
+        while (j >= runstack[num_in_stack] && a[j] > key) {  
+          a[j + 1] = a[j];  
+          j = j - 1;  
+        }  
+
+        a[j + 1] = key;  
+        
+        continue;
+      }
+      //If the run is greater or equal to minrun
+      else if((z - runstack[num_in_stack]) >= RUN){
+        num_in_stack++;
+        runstack[num_in_stack] = z;
+        
+        secondLast = runstack[num_in_stack - 1] - runstack[num_in_stack - 2];
+        Last = runstack[num_in_stack] - runstack[num_in_stack - 1];
+        
+        while(secondLast <= Last && num_in_stack > 1){
+          //Merge
+          l = runstack[num_in_stack - 2];
+          m = runstack[num_in_stack - 1];
+          r = z;
+
+          for(i = m - 1; i >= l; i--){
+            a_tmp[i] = a[i];
+          }
+
+          i = l;
+          j = m;
+
+          for(key = l; key < r; key++){
+            if(i < m && j < r){
+              if(a_tmp[i] < a[j]){
+                a[key] = a_tmp[i++];
+              }
+              else{
+                a[key] = a[j++];
+              }
+            }
+            else{
+              if(j >= r ){
+                a[key] = a_tmp[i++];
+              }
+              else if(i >= m){
+                break;
+              }
+            }
+      
+          }
+
+          num_in_stack--;
+          runstack[num_in_stack] = z;
+
+          secondLast = runstack[num_in_stack - 1] - runstack[num_in_stack - 2];
+          Last = runstack[num_in_stack] - runstack[num_in_stack - 1];
+        }
+        
+      }
+    } 
+    
+  }
+
+  num_in_stack++;
+  runstack[num_in_stack] = n;
+
+  while(num_in_stack > 1){
+    //Merge
+    l = runstack[num_in_stack - 2];
+    m = runstack[num_in_stack - 1];
+    r = n;
+    
+    for(i = m - 1; i >= l; i--){
+      a_tmp[i] = a[i];
+    }
+    
+    i = l;
+    j = m;
+    
+    for(key = l; key < r; key++){
+      if(i < m && j < r){
+        if(a_tmp[i] < a[j]){
+          a[key] = a_tmp[i++];
+        }
+        else{
+          a[key] = a[j++];
+        }
+      }
+      else{
+        if(j >= r ){
+          a[key] = a_tmp[i++];
+        }
+        else if(i >= m){
+          break;
+        }
+      }
+      
+    }
+    
+    num_in_stack--;
+    runstack[num_in_stack] = n;
+  }
+}// end function
+
+#endif 
---
+++ 1. Sorting and Top-Down Splay Trees/topdownsplay.cpp
@@ -0,0 +1,223 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+typedef struct Node Data_Node;
+struct Node {
+	int code;
+	char zip[12];
+	char city[150];
+	char state[6];
+};
+struct Tree_node{
+    int key;
+    Tree_node* lchild;
+    Tree_node* rchild;
+    Data_Node* data;
+};
+
+class SplayTree{
+    public:
+
+        SplayTree(){
+            }
+
+            // RR(Y rotates to the right
+        Tree_node* Right_Rotate(Tree_node* k2){
+            // Single rotate the root k2 to the right
+            // You have to implement this, return the new root
+            Tree_node* k1 = k2->lchild;
+            k2->lchild = k1->rchild;
+            k1->rchild = k2;
+            return k1;
+
+        }
+            // LL(Y rotates to the left)
+        Tree_node* Left_Rotate(Tree_node* k2){
+            // Single rotate the root k2 to the left
+            // You have to implement this, return the new root.
+            Tree_node* k1 = k2->rchild;
+            k2->rchild = k1->lchild;
+            k1->lchild = k2;
+            return k1;
+        }
+
+            // An implementation of top-down splay tree
+
+        Tree_node* Splay(int key, Tree_node* root)
+        {//This is one of the things you have to implement
+
+            if (!root)
+                return NULL;
+            Tree_node Tree;
+            /* .......*/
+            Tree.lchild = NULL;
+            Tree.rchild = NULL;
+
+            Tree_node* LeftTreeMax = &Tree;
+            Tree_node* RightTreeMin = &Tree;
+
+            while(1){
+                if(key < root->key){
+                    if(!root->lchild){
+                        break;
+                    }
+
+                    if(key < root->lchild->key){
+                        root = Right_Rotate(root);
+                        if(!root->lchild){
+                            break;
+                        }
+                    }
+
+                    RightTreeMin->lchild = root;
+                    RightTreeMin = RightTreeMin->lchild;
+                    root = root->lchild;
+                    RightTreeMin->lchild = NULL;
+                }
+                else if(key > root->key){
+                    if(!root->rchild){
+                        break;
+                    }
+
+                    if(key > root->rchild->key){
+                        root = Left_Rotate(root);
+                        if(!root->rchild){
+                            break;
+                        }
+                    }
+
+                    LeftTreeMax->rchild = root;
+                    LeftTreeMax = LeftTreeMax->rchild;
+                    root = root->rchild;
+                    LeftTreeMax->rchild = NULL;
+                }
+                else{
+                    break;
+                }
+            }
+
+            LeftTreeMax->rchild = root->lchild;
+            RightTreeMin->lchild = root->rchild;
+            root->lchild = Tree.rchild;
+            root->rchild = Tree.lchild;
+            return root;
+ 
+        }
+
+        Tree_node* New_Node(int key) {
+            Tree_node*  my_node = new Tree_node;
+            if (!my_node){
+                fprintf(stderr, "Out of memory!\n");
+                exit(1);
+            }
+            my_node->key = key;
+            my_node->lchild = my_node->rchild = NULL;
+            return my_node;
+        }
+
+        Tree_node* Insert(int key, Tree_node* root){
+            Tree_node* my_node = NULL;
+            
+            my_node = New_Node(key);
+            if (!root){
+                root = my_node;
+                return root;
+            }
+            root = Splay(key, root);
+            if (key < root->key){
+                    my_node->lchild = root->lchild;
+                    my_node->rchild = root;
+                    root->lchild = NULL;
+                    root = my_node;
+            }
+            else if (key > root->key){
+                    my_node->rchild = root->rchild;
+                    my_node->lchild = root;
+                    root->rchild = NULL;
+                    root = my_node;
+                }
+            else { /* it's already in tree, don't add it again*/
+                delete my_node;
+                return root;
+            }
+            return root;
+        }
+
+        Tree_node* Delete(int key, Tree_node* root){
+            Tree_node* temp;
+            if (!root)
+                return NULL;
+            root = Splay(key, root);
+            if (key != root->key)
+                return root;
+            else{
+                if (!root->lchild){
+                    temp = root;
+                    root = root->rchild;
+                }
+                else {
+                    temp = root;
+                    root = Splay(key, root->lchild);
+                    root->rchild = temp->rchild;
+                }
+                delete temp;
+                return root;
+            }
+
+        }
+
+        Tree_node* Search(int key, Tree_node* root){
+                return Splay(key, root);
+        }
+
+        void InOrder(Tree_node* root){
+            if (root){
+                InOrder(root->lchild);
+                cout<< "key: " <<root->key;
+                if(root->lchild)
+                    cout<< " | left child: "<< root->lchild->key;
+                if(root->rchild)
+                    cout << " | right child: " << root->rchild->key;
+
+                cout<< endl;
+                InOrder(root->rchild);
+            }
+        }
+};
+
+int main()
+    {
+        SplayTree st;
+        int vector[10] = {9,8,7,6,5,4,3,2,1,0};
+        Tree_node *root;
+        root = NULL;
+        const int length = 10;
+        int i;
+
+        for(i = 0; i < length; i++)
+            root = st.Insert(vector[i], root);
+
+        cout<<"\nInOrder: \n";
+
+        st.InOrder(root);
+        int input, choice;
+        input = 6;
+        root = st.Search(input, root);
+        cout<<"\nAfter Search "<<input<<endl;
+        st.InOrder(root);
+
+        input = 3;
+        root = st.Search(input, root);
+        cout<<"\nAfter Search "<<input<<endl;
+        st.InOrder(root);
+
+        input = 8;
+        root = st.Delete(input,root);
+        cout << "\nAfter Delete "<<input<<endl;
+        st.InOrder(root);
+        cout<<"\n";
+        return 0;
+    }
